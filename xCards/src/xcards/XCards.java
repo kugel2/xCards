de4771ab354742c022e2f98b17558a0992c03332
Florian W. <github.9.kugelfisch2@xoxy.net>
2012-04-22 16:53:22 +0200

    initial commit

---
+++ xCards/src/xcards/XCards.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xcards;
+
+/**
+ *
+ * @author florian
+ */
+public class XCards {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
